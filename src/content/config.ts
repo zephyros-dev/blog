d3165c9c48f66dadc4d91c9a74abe0ea1c38a4e6
zephyros-dev <phong.ngthanh@outlook.com>
2025-07-13 11:44:56 +0700

    fix: missing image properties from Collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -6,6 +6,7 @@ const posts = defineCollection({
     title: z.string(),
     description: z.string(),
     date: z.coerce.date(),
+    image: z.string().default("/static/blog-placeholder.jpeg")
   }),
 });
 
